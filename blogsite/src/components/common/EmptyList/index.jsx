98968920524744c951255d6a73909f6d9be4e6c2
sehr <sehr.sehr1112@gmail.com>
2023-07-24 23:24:40 +0500

    Updated searchbox

---
+++ blogsite/src/components/common/EmptyList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const EmptyList = () => {
+  return (
+    <div className='emptyList-wrap'>EmptyList
+    <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" alt="emptyList" />
+    </div>
+  )
+}
+
+export default EmptyList
--- blogsite/src/pages/Home/index.jsx
+++ blogsite/src/pages/Home/index.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../../components/Home/Header';
 import SearchBar from '../../components/Home/SearchBar';
 import BlogList from '../../components/Home/BlogList';
 import {blogList} from '../../config/data';
+import EmptyList from '../../components/common/EmptyList';
  const Home = () => {
+    const[blogs,setBlogs]= useState(blogList);
+    const [searchKey , setSearchKey] = useState('');
+    // Submit
+    const handleSearchBar = (event) => {
+        event.preventDefault();
+        handleSearchResult();
+    }
+    // search for blog categories
+    const handleSearchResult = () => {
+        const allBlogs = blogList;
+        const filteredBlogs = allBlogs.filter((blog) => blog.category.toLowerCase().includes(searchKey.toLowerCase().trim()));
+        setBlogs(filteredBlogs);
+    }
+
+    const handleClearSearch = () => {
+        setSearchKey('');
+        setBlogs(blogList);
+    }
   return (
     <div> 
     {/* Page header */}
 
     <Header/>
     {/* Search Bar */}
-    <SearchBar/>
+    <SearchBar 
+    value={searchKey} 
+    clearSearch ={ handleClearSearch}
+    formSubmit={handleSearchBar} 
+    handleSearchKey={ (e) => setSearchKey(e.target.value)}/>
     {/* BLogList and EmptyList */}
-    <BlogList blogs={blogList}/>
+    {!blogs.length ? <EmptyList/> :<BlogList blogs={blogs}/>}
     </div>
   )
 };
