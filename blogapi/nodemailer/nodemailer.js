8f9b868109e95b98dd88e875ae7e55fad60877f6
sehr <sehr.sehr1112@gmail.com>
2023-07-29 23:53:12 +0500

    update files

---
+++ blogapi/nodemailer/nodemailer.js
@@ -0,0 +1,14 @@
+const nodemailer = require("nodemailer");
+let transporter = nodemailer.createTransport(
+    {
+        host:'smtp.gmail.com',
+        port:'587',
+        secure:false,
+            auth:{
+                user:"",
+                pass:"",
+            }
+        
+    }
+)
+module.exports = {transporter};
--- blogapi/routes/register.js
+++ blogapi/routes/register.js
@@ -13,17 +13,52 @@
 
 var express = require('express');
 var router = express.Router();
+const multer = require('multer'); //importing multer for file upload
+const {transporter}= require('../nodemailer/nodemailer')
 
+var storage = multer.diskStorage({
+  destination:function(req,file,cb){ //destination for file upload
+    cb(null,'./public/images/');
+
+  },
+  filename:function(req,file,cb){ //filename for file upload
+    cb(null,Date.now()+file.originalname);}
+})
+
+
+var upload = multer({storage:storage}); //uploading file
+
+ 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post('/',upload.single('profileimage'), function(req, res, next) {
   
-  const name=req.body.name;
+    const name=req.body.name;
     const password=req.body.password;
+    const email=req.body.email;
+    const file=req.file.filename;
 
-    console.log(name,password);
-    res.send("hello");
-    res.send(name);
+    const mailOption={
+      from:`Sehrish Saddique <sehr.sehr1112@gmail.com>`,
+      to:email,
+      subject:"You are amazing developer ",
+      html:`<h1>Thank you for registering</h1>`
+      
+    }
+    mailOption.returnInfo = true;
 
-});
+    transporter.sendMail(mailOption,(error,info)=>{
+      
+         if (error) {
+          throw error; // Handle the error, or use proper error handling instead of throwing.
+        } else {
+          // Access the 'info' object to get information about the sent email
+          console.log("Email sent: " + info.response); // 'info.response' contains the response message from the email server.
+          //
+       
+    }});
+    console.log(name,password,file);
+   
+
+})
 
 module.exports = router;
